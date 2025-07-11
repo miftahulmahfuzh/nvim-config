725bfb3f32969fdf6be418bd16b4be7a5fb4e781
miftahulmahfuzh <mahfuzh74@gmail.com>
2025-07-11 16:07:17 +0700

    parsed avante window as markdown

--- lua/config/treesitter.lua
+++ lua/config/treesitter.lua
@@ -1,10 +1,29 @@
 -- /home/devmiftahul/.config/nvim/lua/config/treesitter.lua
 
 require("nvim-treesitter.configs").setup {
-  ensure_installed = { "go", "markdown", "python", "http", "cpp", "lua", "vim", "json", "toml" },
+  ensure_installed = {
+    "python", "http", "cpp", "lua", "vim", "json", "toml",
+    "markdown", "markdown_inline", "html", "css", "javascript", "go"
+  },
   ignore_install = {}, -- List of parsers to ignore installing
   highlight = {
     enable = true, -- false will disable the whole extension
     disable = { "help" }, -- list of language that will be disabled
+    additional_vim_regex_highlighting = { "markdown" }, -- Enable vim regex highlighting for markdown
+  },
+  -- Add incremental selection for better markdown editing
+  incremental_selection = {
+    enable = true,
+    keymaps = {
+      init_selection = "gnn",
+      node_incremental = "grn",
+      scope_incremental = "grc",
+      node_decremental = "grm",
+    },
+  },
+  -- Add indentation support
+  indent = {
+    enable = true,
+    disable = { "yaml" }, -- YAML indentation can be problematic
   },
 }
