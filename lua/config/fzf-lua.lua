00b9162ca9504f8cdfe79b418ba632a853fb9be1
miftahulmahfuzh <mahfuzh74@gmail.com>
2025-07-29 12:23:46 +0800

    added fzf recent files

--- lua/config/fzf-lua.lua
+++ lua/config/fzf-lua.lua
@@ -1,5 +1,3 @@
--- /home/devmiftahul/.config/nvim/lua/config/fzf-lua.lua
-
 local actions = require("fzf-lua.actions")
 local opencode = require("custom.opencode") -- Require our new, clean module
 
@@ -25,6 +23,7 @@ require("fzf-lua").setup({
 
 -- Standard fzf-lua keymaps. Clean, simple, effective.
 vim.keymap.set("n", "<leader>ff", "<cmd>FzfLua files<cr>", { desc = "Fuzzy find files" })
+vim.keymap.set("n", "<leader>fe", "<cmd>FzfLua oldfiles<cr>", { desc = "Fuzzy find recent files" })
 vim.keymap.set("n", "<leader>fg", "<cmd>FzfLua live_grep<cr>", { desc = "Fuzzy grep files" })
 vim.keymap.set("n", "<leader>fh", "<cmd>FzfLua helptags<cr>", { desc = "Fuzzy grep tags in help files" })
 vim.keymap.set("n", "<leader>fr", "<cmd>FzfLua btags<cr>", { desc = "Fuzzy search buffer tags" })
